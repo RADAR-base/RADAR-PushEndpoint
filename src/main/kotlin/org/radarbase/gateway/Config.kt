ae7d203af6706dc732af49c624ea7413e1817f91
yatharthranjan <yatharthranjan89@gmail.com>
2021-02-03 12:35:24 +0000

    add support for multiple users in same request
    - Update dependencies

--- src/main/kotlin/org/radarbase/gateway/Config.kt
+++ src/main/kotlin/org/radarbase/gateway/Config.kt
@@ -48,10 +48,10 @@ data class GarminConfig(
     val backfill: BackfillConfig = BackfillConfig(),
     val userRepositoryClass: String =
         "org.radarbase.push.integration.garmin.user.GarminServiceUserRepository",
-    val userRepositoryUrl: String = "",
+    val userRepositoryUrl: String = "http://localhost:8080/",
     val userRepositoryClientId: String = "radar_pushendpoint",
     val userRepositoryClientSecret: String = "",
-    val userRepositoryTokenUrl: String = "",
+    val userRepositoryTokenUrl: String = "http://localhost:8080/token/",
     val dailiesTopicName: String = "push_garmin_daily_summary",
     val activitiesTopicName: String = "push_garmin_activity_summary",
     val activityDetailsTopicName: String = "push_garmin_activity_detail",
--- src/main/kotlin/org/radarbase/push/integration/garmin/auth/GarminAuthValidator.kt
+++ src/main/kotlin/org/radarbase/push/integration/garmin/auth/GarminAuthValidator.kt
@@ -7,6 +7,7 @@ import org.radarbase.jersey.auth.AuthValidator
 import org.radarbase.jersey.auth.disabled.DisabledAuth
 import org.radarbase.jersey.exception.HttpUnauthorizedException
 import org.radarbase.push.integration.common.auth.DelegatedAuthValidator.Companion.GARMIN_QUALIFIER
+import org.radarbase.push.integration.common.user.User
 import org.radarbase.push.integration.garmin.user.GarminUserRepository
 import org.slf4j.LoggerFactory
 import javax.inject.Named
@@ -19,39 +20,65 @@ class GarminAuthValidator(
     @Named(GARMIN_QUALIFIER) private val userRepository: GarminUserRepository
 ) :
     AuthValidator {
-    override fun verify(token: String, request: ContainerRequestContext): Auth? {
+
+    override fun verify(token: String, request: ContainerRequestContext): Auth {
         return if (token.isBlank()) {
-            throw HttpUnauthorizedException("invalid_token", "The token was not empty")
+            throw HttpUnauthorizedException("invalid_token", "The token was empty")
         } else {
+            var isAnyUnauthorised = false
             // Enrich the request by adding the User
             // the data format in Garmin's post is { <data-type> : [ {<data-1>}, {<data-2>} ] }
             val tree = request.getProperty("tree") as JsonNode
-            val userId = tree[tree.fieldNames().next()][0][USER_ID_KEY]
-                ?: throw HttpUnauthorizedException("invalid_user", "No user id provided")
-            val user = try {
-                userRepository.findByExternalId(userId.asText())
-            } catch (exc: NoSuchElementException) {
-                throw HttpUnauthorizedException(
-                    "no_user", "The user could not be found in the " +
-                            "user repository"
-                )
-            }
 
-            if (!user.isAuthorized) {
-                throw HttpUnauthorizedException(
-                    "invalid_user", "The user does not seem to be authorized"
-                )
-            }
+            val userTreeMap: Map<User, JsonNode> =
+                // group by user ID since request can contain data from multiple users
+                tree[tree.fieldNames().next()].groupBy { node ->
+                    node[USER_ID_KEY].asText()
+                }.filter { (k, v) ->
+                    val user = try {
+                        userRepository.findByExternalId(k)
+                    } catch (exc: NoSuchElementException) {
+                        isAnyUnauthorised = true
+                        logger.warn(
+                            "no_user: {}", "The user $k could not be found in the " +
+                                "user repository."
+                        )
+                        return@filter false
+                    }
+                    if (!user.isAuthorized) {
+                        userRepository.reportDeregistration(user)
+                        isAnyUnauthorised = true
+                        logger.warn(
+                            "invalid_user: {}", "The user $k does not seem to be authorized."
+                        )
+                        return@filter false
+                    }
+                    if (userRepository.getAccessToken(user) != v[0][USER_ACCESS_TOKEN_KEY].asText()
+                    ) {
+                        isAnyUnauthorised = true
+                        userRepository.reportDeregistration(user)
+                        logger.warn(
+                            "invalid_user: {}", "The token for user $k does not" +
+                                " match with the records on the system."
+                        )
+                        return@filter false
+                    }
+                    true
+                }.mapKeys { (k, _) ->
+                    userRepository.findByExternalId(k)
+                }.mapValues { (_, v) ->
+                    // Map the List<JsonNode> back to <data-type>: [ {<data-1>}, {<data-2>} ]
+                    // so it can be processed in the services without much refactoring
+                    objectMapper.createObjectNode()
+                        .set(tree.fieldNames().next(), objectMapper.valueToTree(v))
+                }
 
-            // Compare access tokens for authenticity of the request
-            if (userRepository.getAccessToken(user) == token) {
-                request.setProperty("user", user)
-            } else {
-                throw HttpUnauthorizedException(
-                    "invalid_user", "The token for user ${user.id} does not" +
-                            " match with the records on the system"
-                )
-            }
+            request.setProperty("user_tree_map", userTreeMap)
+            request.setProperty(
+                "auth_metadata",
+                mapOf("isAnyUnauthorised" to isAnyUnauthorised.toString())
+            )
+            request.removeProperty("tree")
 
             // Disable auth since we don't have proper auth support
             DisabledAuth("res_gateway")
