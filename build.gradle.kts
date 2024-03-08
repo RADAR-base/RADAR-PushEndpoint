c7d68a48c5d0a41b822b6115e98b845c1b5f039c
this-Aditya <adityamishra2217@gmail.com>
2024-03-08 14:44:54 +0530

    Build, gradle and dependencies updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,9 +87,9 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "11"
-        apiVersion = "1.5"
-        languageVersion = "1.5"
+        jvmTarget = "17"
+        apiVersion = "1.8"
+        languageVersion = "1.8"
     }
 }
 
@@ -189,5 +189,5 @@ tasks.withType<DependencyUpdatesTask> {
 }
 
 tasks.wrapper {
-    gradleVersion = "7.1"
+    gradleVersion = "8.3"
 }
