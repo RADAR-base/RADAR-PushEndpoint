b8722b76c4ccf1cad1f2e07949782d60312f59ef
this-Aditya <adityamishra2217@gmail.com>
2026-06-05 21:41:19 +0530

    Update source type name

--- src/main/kotlin/org/radarbase/push/integration/google/user/GoogleHealthServiceUserRepository.kt
+++ src/main/kotlin/org/radarbase/push/integration/google/user/GoogleHealthServiceUserRepository.kt
@@ -207,7 +207,7 @@ class GoogleHealthServiceUserRepository(
     }
 
     companion object {
-        private const val GOOGLEHEALTH_SOURCE = "Google"
+        private const val GOOGLEHEALTH_SOURCE = "GoogleHealth"
         private val JSON_MEDIA_TYPE = "application/json; charset=utf-8".toMediaType()
         private val EMPTY_BODY: RequestBody = "".toRequestBody(JSON_MEDIA_TYPE)
 
